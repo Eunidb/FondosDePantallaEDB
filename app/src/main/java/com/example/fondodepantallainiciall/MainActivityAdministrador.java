d7c5b0cf130e19b62a6bd6a95d5a1d5ff8fd4e0c
dianaandrade2022 <a3521110157@alumno.uttehuacan.edu.mx>
2024-05-31 00:03:12 -0600

    by andrade

---
+++ app/src/main/java/com/example/fondodepantallainiciall/MainActivityAdministrador.java
@@ -0,0 +1,14 @@
+package com.example.fondodepantallainiciall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityAdministrador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_administrador);
+    }
+}
